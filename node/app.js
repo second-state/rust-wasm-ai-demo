62d63a25d51c316f9c8f47ee01a4f607ef9c28b3
Michael Yuan <michael@secondstate.io>
2020-05-17 22:51:04 +0000

    Remove dep on nodejs-helper

--- node/app.js
+++ node/app.js
@@ -1,7 +1,12 @@
 const { infer } = require('../pkg/csdn_ai_demo_lib.js');
 
-var result = JSON.parse(infer(JSON.stringify(["mobilenet_v2_1.4_224_frozen.pb", "grace_hopper.jpg", 224, 224])));
+const fs = require('fs');
+var data_model = fs.readFileSync("mobilenet_v2_1.4_224_frozen.pb");
+var data_img_cat = fs.readFileSync("cat.png");
+var data_img_hopper = fs.readFileSync("grace_hopper.jpg");
+
+var result = JSON.parse( infer(data_model, data_img_hopper, 224, 224) );
 console.log("Detected object id " + result[1] + " with probability " + result[0]);
 
-var result = JSON.parse(infer(JSON.stringify(["mobilenet_v2_1.4_224_frozen.pb", "cat.png", 224, 224])));
+var result = JSON.parse( infer(data_model, data_img_cat, 224, 224) );
 console.log("Detected object id " + result[1] + " with probability " + result[0]);
--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,35 @@
 use wasm_bindgen::prelude::*;
 use tract_tensorflow::prelude::*;
-use nodejs_helper;
 use std::io::Cursor;
 
 #[wasm_bindgen]
-pub fn infer(params: &str) -> String {
-    let ps: (String, String, usize, usize) = serde_json::from_str(params).unwrap();
-    let model_filename = &ps.0;
-    let image_filename = &ps.1;
-    let image_height = ps.2;
-    let image_width = ps.3;
-
-    nodejs_helper::console::log(&format!("Model: {:?}", model_filename));
-    nodejs_helper::console::log(&format!("Image: {:?}", image_filename));
-    let model_data = &nodejs_helper::fs::read_file_sync(model_filename);
-    let image_data = &nodejs_helper::fs::read_file_sync(image_filename);
-
-    let res: (f32, u32) = infer_impl (model_data, image_data, image_height, image_width).unwrap();
+pub fn infer(model_data: &[u8], image_data: &[u8], image_height: i32, image_width: i32) -> String {
+    let res: (f32, u32) = infer_impl (model_data, image_data, image_height as usize, image_width as usize).unwrap();
     return serde_json::to_string(&res).unwrap();
 }
 
 fn infer_impl (model_data: &[u8], image_data: &[u8], image_height: usize, image_width: usize) -> TractResult<(f32, u32)> {
-    nodejs_helper::console::time("Inference");
     // load the model
     let mut model_data_mut = Cursor::new(model_data);
     let mut model = tract_tensorflow::tensorflow().model_for_read(&mut model_data_mut)?;
-    nodejs_helper::console::time_log("Inference", "Model loaded");
-
+    
     // specify input type and shape
     model.set_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(1, image_height, image_width, 3)))?;
     // optimize the model and get an execution plan
     let model = model.into_optimized()?;
     let plan = SimplePlan::new(&model)?;
-    nodejs_helper::console::time_log("Inference", "Plan loaded");
-
+    
     // open image, resize it and make a Tensor out of it
     let image = image::load_from_memory(image_data).unwrap().to_rgb();
-    nodejs_helper::console::time_log("Inference", "Image loaded");
     let resized = image::imageops::resize(&image, image_height as u32, image_width as u32, ::image::imageops::FilterType::Triangle);
     let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, image_height, image_width, 3), |(_, y, x, c)| {
         resized[(x as _, y as _)][c] as f32 / 255.0
     })
     .into();
-    nodejs_helper::console::time_log("Inference", "Image resized");
-
+    
     // run the plan on the input
     let result = plan.run(tvec!(image))?;
-    nodejs_helper::console::time_log("Inference", "Model applied");
-
+    
     // find and display the max value with its index
     let best = result[0]
         .to_array_view::<f32>()?
@@ -55,7 +37,6 @@ fn infer_impl (model_data: &[u8], image_data: &[u8], image_height: usize, image_
         .cloned()
         .zip(1..)
         .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-    nodejs_helper::console::time_end("Inference");
     match best {
         Some(t) => Ok(t),
         None => Ok((0.0, 0)),
