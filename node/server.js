c9aedea93cb63e046382219c04e237892ab0449b
Dina Basumatary <dina.basumatary@newsperform.com.au>
2020-07-20 19:41:58 +1000

    undo double quotes

--- node/server.js
+++ node/server.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const fileUpload = require("express-fileupload");
-const { infer } = require("../pkg/csdn_ai_demo_lib.js");
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const { infer } = require('../pkg/csdn_ai_demo_lib.js');
 
-const fs = require("fs");
-var data_model = fs.readFileSync("mobilenet_v2_1.4_224_frozen.pb");
+const fs = require('fs');
+var data_model = fs.readFileSync('mobilenet_v2_1.4_224_frozen.pb');
 
 var labels = [];
-fs.readFileSync("imagenet_slim_labels.txt", "utf-8")
+fs.readFileSync('imagenet_slim_labels.txt', 'utf-8')
   .split(/\r?\n/)
   .forEach(function (line) {
     labels.push(line);
   });
 
 const app = express();
-const host = "0.0.0.0";
+const host = '0.0.0.0';
 const port = 8080;
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(fileUpload());
 // app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.redirect("/index.html"));
+app.get('/', (req, res) => res.redirect('/index.html'));
 
-app.post("/infer", function (req, res) {
+app.post('/infer', function (req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send("No files were uploaded.");
+    return res.status(400).send('No files were uploaded.');
   }
   console.log(
-    "Received " +
+    'Received ' +
       req.files.image_file.name +
-      " with size: " +
+      ' with size: ' +
       req.files.image_file.size
   );
 
@@ -37,20 +37,20 @@ app.post("/infer", function (req, res) {
   var result = JSON.parse(infer(data_model, image_file.data, 224, 224));
   console.timeEnd(image_file.name);
 
-  var confidence = "low";
+  var confidence = 'low';
   if (result[0] > 0.75) {
-    confidence = "very high";
+    confidence = 'very high';
   } else if (result[0] > 0.5) {
-    confidence = "high";
+    confidence = 'high';
   } else if (result[0] > 0.2) {
-    confidence = "medium";
+    confidence = 'medium';
   }
   res.send(
-    "Detected <b>" +
+    'Detected <b>' +
       labels[result[1] - 1] +
-      "</b> with <u>" +
+      '</b> with <u>' +
       confidence +
-      "</u> confidence."
+      '</u> confidence.'
   );
 });
 
